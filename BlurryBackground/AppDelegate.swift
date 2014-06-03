de227f4722cddd72d4141126c14d841ed5e0877d
brimelow@adobe.com <brimelow@adobe.com>
2014-06-03 16:12:07 -0700

    Initial Commit

---
+++ BlurryBackground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BlurryBackground
+//
+//  Created by Lee Brimelow on 6/3/14.
+//  Copyright (c) 2014 Lee Brimelow. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BlurryBackground/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BlurryBackground
+//
+//  Created by Lee Brimelow on 6/3/14.
+//  Copyright (c) 2014 Lee Brimelow. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
