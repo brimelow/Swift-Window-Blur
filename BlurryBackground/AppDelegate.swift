70818c4ce7ca5b537cd3618b6be2bae9c10a3fce
brimelow@adobe.com <brimelow@adobe.com>
2014-06-03 16:21:52 -0700

    Initial commit

--- BlurryBackground/AppDelegate.swift
+++ BlurryBackground/AppDelegate.swift
@@ -11,10 +11,22 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
-
+    
+    // define the visual effect view
+    var blurryView = NSVisualEffectView(frame: NSRect(x: 0, y: 0, width: 800, height: 600))
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+        
+        // this is default value but is here for clarity
+        blurryView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
+        
+        // set the background to always be the dark blur
+        blurryView.material = NSVisualEffectMaterial.Dark
+        
+        // set it to always be blurry regardless of window state
+        blurryView.state = NSVisualEffectState.Active
+        
+        self.window.contentView.addSubview(blurryView)
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
